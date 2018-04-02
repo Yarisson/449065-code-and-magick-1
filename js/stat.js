ee3ddd507519257dd65118e4ae45dcb1410ea1b4
Yarisson <gemini8811@gmail.com>
2018-04-03 01:28:11 +0300

    добавление результатов игроков

--- js/stat.js
+++ js/stat.js
@@ -33,21 +33,37 @@ var randomColor = function () {
   return 'rgba(0, 0, 255, ' + Math.random() + ')';
 };
 
+var renderText = function (ctx, color, style, x, y) {
+  ctx.fillStyle = color;
+  ctx.font = style;
+  ctx.fillText('Ура вы победили!', x, y);
+  ctx.fillText('Список результатов:', x, y + GAP_TEXT);     
+};
+
+var renderResult = function (ctx, arrayResults, maxResult) {
+  for (var i = 0; i < arrayResults.length; i++) {
+    ctx.font = '16px PT Mono';
+    ctx.fillStyle = '#green';
+    ctx.fillText(Math.round(arrayResults[i]), X_GISTO_POINT + (GISTO_WIDTH + GISTO_GAP_X) * [i], Y_GISTO_POINT + GISTO_HEIGHT * arrayResults[i] / maxResult - 5);
+  }
+};
+
+var renderGisto = function (ctx, arrayNames, arrayTimes, max) {
+  for (var i = 0; i < arrayNames.length; i++) {
+    ctx.fillStyle = arrayNames[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : randomColor();
+    ctx.fillText(arrayNames[i], X_GISTO_POINT + (GISTO_WIDTH + GISTO_GAP_X) * [i], Y_GISTO_NAME_POINT);
+    ctx.fillRect(X_GISTO_POINT + (GISTO_WIDTH + GISTO_GAP_X) * [i], Y_GISTO_POINT, GISTO_WIDTH, GISTO_HEIGHT * arrayTimes[i] / max);
+  }
+};
+
 window.renderStatistics = function (ctx, names, times) {
 
   renderCloud(ctx, X_CLOUD_POINT + GAP, Y_CLOUD_POINT + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, X_CLOUD_POINT, Y_CLOUD_POINT, '#fff');
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', X_TEXT_POINT, Y_TEXT_POINT);
-  ctx.fillText('Список результатов:', X_TEXT_POINT, Y_TEXT_POINT + GAP_TEXT);
+  renderText(ctx, '#000', '16px PT Mono', X_TEXT_POINT, Y_TEXT_POINT);
 
   var maxTime = getMaxElement(times);
 
-  for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : randomColor();
-    ctx.fillText(names[i], X_GISTO_POINT + (GISTO_WIDTH + GISTO_GAP_X) * [i], Y_GISTO_NAME_POINT);
-    ctx.fillRect(X_GISTO_POINT + (GISTO_WIDTH + GISTO_GAP_X) * [i], Y_GISTO_POINT, GISTO_WIDTH, GISTO_HEIGHT * times[i] / maxTime);
-  }
+  renderResult(ctx, times, maxTime);
+  renderGisto(ctx, names, times, maxTime);
 };
